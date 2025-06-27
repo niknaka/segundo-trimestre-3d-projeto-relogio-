67b7ce246223a4add6de0af5a08c0ee31d8b07ab
niknaka <nakamura.nicolly@escola.pr.gov.br>
2025-06-27 09:40:42 -0300

    aula05

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const botoes = document.querySelectorAll("botao");
+const textos = document.querySelectorAll(".abas-cinteudo");
 console (botoes)
 
 for(i=0;i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(j=0; j<botoes.length; j++){
             botoes[j].classList.remove(ativo);
+            textos[j].classList.remove(ativo);
         }
         botoes[i].classList.add(ativo);
     }
